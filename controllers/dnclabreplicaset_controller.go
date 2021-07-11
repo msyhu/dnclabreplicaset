a9962622ee546cdd5b77eb2f144a8e5aee25caf0
msyhu <msyhu@korea.ac.kr>
2021-07-11 19:55:29 +0900

    log crash 수정

--- controllers/dnclabreplicaset_controller.go
+++ controllers/dnclabreplicaset_controller.go
@@ -24,12 +24,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sort"
 
+	mycorev1 "ds.korea.ac.kr/dnclabreplicaset/api/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	mycorev1 "ds.korea.ac.kr/dnclabreplicaset/api/v1"
 
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -57,7 +55,8 @@ type DnclabReplicaSetReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *DnclabReplicaSetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	//_ = log.FromContext(ctx)
+	_ = context.Background()
 	log := r.Log.WithValues("DnclabReplicaSet", req.NamespacedName)
 
 	// your logic here
