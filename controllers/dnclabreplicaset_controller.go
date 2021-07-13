a1c1a4035d54aa61511fe9fcc5dc9bc5922374b0
msyhu <msyhu@korea.ac.kr>
2021-07-13 15:46:22 +0900

    reconcile 수정 중

--- controllers/dnclabreplicaset_controller.go
+++ controllers/dnclabreplicaset_controller.go
@@ -21,7 +21,6 @@ import (
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"math/rand"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sort"
 	"strconv"
@@ -47,18 +46,9 @@ type DnclabReplicaSetReconciler struct {
 //+kubebuilder:rbac:groups=mycore.ds.korea.ac.kr,resources=dnclabreplicasets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=mycore.ds.korea.ac.kr,resources=dnclabreplicasets/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DnclabReplicaSet object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *DnclabReplicaSetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-	//_ = context.Background()
+	//_ = log.FromContext(ctx)
+	_ = context.Background()
 	drsLogger := r.Log.WithValues("dnclabreplicaset", req.NamespacedName)
 
 	// your logic here
@@ -169,6 +159,7 @@ func (r *DnclabReplicaSetReconciler) Reconcile(ctx context.Context, req ctrl.Req
 func (r *DnclabReplicaSetReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&mycorev1.DnclabReplicaSet{}).
+		Owns(&corev1.Pod{}).
 		Complete(r)
 }
 
